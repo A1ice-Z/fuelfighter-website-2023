ac845cc0ab757fb39cb95f4b86c811cfb1c79921
andrerit <andrerit@stud.ntnu.no>
2022-10-31 17:35:21 +0100

    basic

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -12,12 +12,10 @@ export default function Header() {
                 </a>
                 <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="#" class="">Fuel Fighter</a></li>
-                    <li><a href="#" class="">Home</a></li>
-                    <li><a href="#" class="">Features</a></li>
-                    <li><a href="#" class="">Pricing</a></li>
-                    <li><a href="#" class="">FAQs</a></li>
+                    <li><a href="#" class="">Blog</a></li>
                     <li><a href="#" class="">About</a></li>
-                    <li><a href="#" class="">blog</a></li>
+                    <li><a href="#" class="">Team</a></li>
+                    <li><a href="#" class="">Sponsors</a></li>
                     <li><a href="#" class="">dev</a></li>
                 </ul>
             
