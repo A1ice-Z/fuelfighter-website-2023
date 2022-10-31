f509723fe41c8faa1336bb4e45dab8624311e3e0
olavlha <olavlha@stud.ntnu.no>
2022-10-31 18:19:13 +0100

    Styling structure
    Added files for styling the team page and the title component

---
+++ client/src/components/Team/TeamTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '.../styles/componentStyles/Team/TeamTitle.css'
+
+
+export default function TeamTitle() {
+    return(
+
+    <> 
+    
+    </>
+
+    )
+}
--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import '../styles/pageStyles/Team.css'
+
 
 export default function Team() {
     return(
