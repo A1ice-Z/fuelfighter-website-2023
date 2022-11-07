2c52b49bcde6a5320bd96fafe93a5adf167d012c
andrerit <andrerit@stud.ntnu.no>
2022-11-07 19:01:16 +0100

    404

--- client/src/pages/404.jsx
+++ client/src/pages/404.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import "../styles/pageStyles/404.css"
+
+import img404 from "../assets/404-img.jpeg"
 
 export default function PageNotFound() {
     return(
-        <div>
-            <h1>Hei! This page doesn't exist!</h1>
-            <h2><a href="/">go back to the home page!</a></h2>
+        <div className="container404">
+            <img class="bi me-2" id="img404" src={img404}/>
+            <div><h2>We are having a problem charging this page!</h2></div>
+            <div className="buttons404">
+                <a href="/"><button type="button" class="btn btn-primary">Go to Main Page</button></a>
+                <a href="/contact"><button type="button" class="btn btn-secondary">Go to Contact Us</button></a>
+            </div>
         </div>
     )
 }
