e052b24883450636961a5ed5084178840ea94015
andrerit <andrerit@stud.ntnu.no>
2022-11-13 14:46:24 +0100

    structure for the values added in the main

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,47 @@ import React from "react"
 import "../styles/pageStyles/home.css"
 import video from '../assets/video/mainPageVideo.mp4';
 
+import imgValues from '../assets/home/value1.jpg'
+
 export default function Home() {
     return(
         <div className="homeContainer">
             <div className="homeFirst">
                 <video src={video} controls={false} playing={true} autoPlay muted loop></video>                
             </div>
-            <div className="homeSecond">
-                About
+            <div className="homeAbout">
+                <div id="homeSection">
+                    <div className="testoHomeSections">
+                        <h1>Our values</h1>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div className="valuesImgContainer">
+                            <img id="imgHomeAbout" alt="img values " src={imgValues}/>
+                    </div>
+                </div>
+
+                <div id="homeSectionInverted">
+                    <div className="valuesImgContainer">
+                            <img id="imgHomeAboutInv" alt="img values " src={imgValues}/>
+                    </div>
+                    <div className="testoHomeSections">
+                        <h1>Our values</h1>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+
+                <div id="homeSection">
+                    <div className="testoHomeSections">
+                        <h1>Our values</h1>
+                        <p></p>
+                        <p></p>
+                    </div>
+                    <div className="valuesImgContainer">
+                            <img id="imgHomeAbout" alt="img values " src={imgValues}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
