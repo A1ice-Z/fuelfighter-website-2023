6c74d94a44fb8885a19ad327143ff275ab30ce29
andrerit <andrerit@stud.ntnu.no>
2023-01-10 18:07:12 +0100

    home & blog

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,14 +8,16 @@ export default function Home() {
     return(
         <div className="homeContainer">
             <div className="homeFirst">
-                <video src={video} controls={false} playing={true} autoPlay muted loop></video>                
+                <video src={video} controls={false} playing={true} autoPlay muted loop />
+                <div className="homeTitle">
+					<h1>Fuel Fighter NTNU</h1>
+				</div>
             </div>
             <div className="homeAbout">
                 <div id="homeSection">
                     <div className="testoHomeSections">
-                        <h1>Our values</h1>
-                        <p></p>
-                        <p></p>
+                        <h1>Inspire a sustainable future</h1>
+                        <p>Our Motto</p>
                     </div>
                     <div className="valuesImgContainer">
                             <img id="imgHomeAbout" alt="img values " src={imgValues}/>
@@ -27,7 +29,7 @@ export default function Home() {
                             <img id="imgHomeAboutInv" alt="img values " src={imgValues}/>
                     </div>
                     <div className="testoHomeSections">
-                        <h1>Our values</h1>
+                        <h1>Our Mission</h1>
                         <p></p>
                         <p></p>
                     </div>
@@ -35,7 +37,7 @@ export default function Home() {
 
                 <div id="homeSection">
                     <div className="testoHomeSections">
-                        <h1>Our values</h1>
+                        <h1>Our Values</h1>
                         <p></p>
                         <p></p>
                     </div>
