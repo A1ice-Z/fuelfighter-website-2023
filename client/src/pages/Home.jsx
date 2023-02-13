23d4a0a7ac16c1f04d06186cc40779b77ac33094
Alice <alicezheng888@gmail.com>
2023-02-13 20:02:09 +0100

    jolo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,44 +6,44 @@ import video from '../assets/video/mainPageVideo.mp4';
 import imgValues from '../assets/pages/home/value1.jpg'
 
 export default function Home() {
-    return(
+    return (
         <div className="homeContainer">
             <div className="homeFirst">
                 <video src={video} controls={false} playing={true} autoPlay muted loop />
                 <div id="homeTitle" className="homeTitle">
-					<h1>Fuel Fighter NTNU</h1>
-				</div>
+                    <h1>Fuel Fighter NTNU</h1>
+                </div>
             </div>
             <div className="homeAbout">
                 <div id="homeSection">
                     <div className="testoHomeSections">
                         <h1>Inspire a sustainable future</h1>
-                        <p>Our Motto</p>
+                        <p>Our Motto and Values</p>
                     </div>
                     <div className="valuesImgContainer">
-                            <img id="imgHomeAbout" alt="img values " src={imgValues}/>
+                        <img id="imgHomeAbout" alt="img values " src={imgValues} />
                     </div>
                 </div>
 
                 <div id="homeSectionInverted">
                     <div className="valuesImgContainer">
-                            <img id="imgHomeAboutInv" alt="img values " src={imgValues}/>
+                        <img id="imgHomeAboutInv" alt="img values " src={imgValues} />
                     </div>
                     <div className="testoHomeSections">
                         <h1>Our Mission</h1>
-                        <p></p>
+                        <p>to make things sustainable</p>
                         <p></p>
                     </div>
                 </div>
 
                 <div id="homeSection">
                     <div className="testoHomeSections">
-                        <h1>Our Values</h1>
-                        <p></p>
+                        <h1>Our Goals</h1>
+                        <p>Fn-målene</p>
                         <p></p>
                     </div>
                     <div className="valuesImgContainer">
-                            <img id="imgHomeAbout" alt="img values " src={imgValues}/>
+                        <img id="imgHomeAbout" alt="img values " src={imgValues} />
                     </div>
                 </div>
             </div>
