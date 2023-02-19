872f107219e04768e4ebe315a7998f7197549ed5
patrykku <patrykku@stud.ntnu.no>
2023-02-19 15:12:33 +0100

    Team names and description formatted

--- client/src/pages/NewTeam.jsx
+++ client/src/pages/NewTeam.jsx
@@ -1,26 +1,73 @@
-import React from "react"
+import React, { useEffect, useState } from 'react'
 import "../styles/pageStyles/NewTeam.css"
+import teamService from '../_services/team.service'
 
 
 export default function NewTeam() {
+    const defaultYear = 2022; // The default year
+    const defaultTeam = 0; // The default index
+    const [selectedYear, setSelectedYear] = useState(defaultYear);
+    const [selectedTeam, setSelectedTeam] = useState(defaultTeam);
+    const [displayTeam, setDisplayTeam] = useState([]);
+
+
+    const onClickChangeYear = (year) => {
+        setSelectedYear(year)    
+    }
+    
+    const onClickChangeTeam = (team) => {
+        setSelectedTeam(team)    
+    }
+
+    useEffect( () => {
+        var showYear = selectedYear;
+        var showTeam = selectedTeam;
+
+        const loadingData = async (showYear, showTeam) => {
+            const data = await teamService.team(showYear, showTeam);
+            setDisplayTeam([data])
+        }
+        loadingData(showYear, showTeam);
+    }, [selectedTeam, selectedYear])
     return(
-        <div class="container" id="maincont">
+        <div class="container" id="teamContainer">
             <div class="row">
                 <div class="col-2" id="row1">
                     <h1>Years </h1>
                 </div>
                 <div class="col-10" id="row1">
-                    <div class="row" id="row1">
-                        <h1>Title </h1>
+                    <div class="row" id="teamTitleRow">
+                        <h1 id="teamTitleText">Our Team </h1>
                     </div>
-                    <div class="row" id="row1">
-                        <h1>Different team choice </h1>
+                    <div class="row justify-content-center" id="teamGroupRow">
+                    
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Board</p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Autonomous </p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Design </p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Electrical </p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Marketing & Finance</p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Mechanical </p>
+                        </div>
+                        <div class="col-auto" id="chooseGroupButton">
+                            <p id="chooseGroupButtonText">Software </p>
+                        </div>
                     </div>
-                    <div class="row" id="row1">
-                        <h1>Team description </h1>
+                    <div class="row" id="teamdescriptionRow">
+                        <p>Team description </p>
                     </div>
-                    <div class="row" id="row1">
-                        <h1>Pictures </h1>
+                    <div class="row" id="teamPicturesRow">
+                        <h1> Pictures </h1>
                     </div>
                 </div>
             </div>
