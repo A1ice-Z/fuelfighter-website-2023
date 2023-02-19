8eca8f9bf88162b3740130a454ccbd7e48a14561
sofia <Sofiakam@stud.ntnu.no>
2023-02-19 15:12:39 +0100

    made the years

---
+++ client/src/pages/NewTeamYears.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import '../styles/pageStyles/NewTeamYears.css'
+
+
+
+export default function YearsInTeam() {
+
+    
+
+return(
+
+    <div className='YearsSection'>
+
+        <div className="yearTitle1"> 
+            <p>Team 2023</p>
+        </div>
+        <div className="yearTitle2"> 
+            <p>Team 2022</p>
+        </div>
+        <div className="yearTitle3">
+            <p>Team 2021</p>
+        </div>
+        <div className="yearTitle4"> 
+            <p>Team 2020</p>
+        </div>
+
+    </div>
+        
+        
+    )
+}
