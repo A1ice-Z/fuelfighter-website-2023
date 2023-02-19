b46a9bd5cd3e8fb70a845f0be563d03c50581b54
patrykku <patrykku@stud.ntnu.no>
2023-02-19 12:31:23 +0100

    Page layout for new team page

--- client/src/pages/NewTeam.jsx
+++ client/src/pages/NewTeam.jsx
@@ -4,31 +4,25 @@ import "../styles/pageStyles/NewTeam.css"
 
 export default function NewTeam() {
     return(
-        <div class="container-xxl">
-            <div class="row-6">
-                <div class="col-sm" id="row1">
-                    <h1>Team Name </h1>
+        <div class="container" id="maincont">
+            <div class="row">
+                <div class="col-2" id="row1">
+                    <h1>Years </h1>
                 </div>
-                <div class="col-sm" id="col1">
-                    <h1>Team Name</h1>
+                <div class="col-10" id="row1">
+                    <div class="row" id="row1">
+                        <h1>Title </h1>
+                    </div>
+                    <div class="row" id="row1">
+                        <h1>Different team choice </h1>
+                    </div>
+                    <div class="row" id="row1">
+                        <h1>Team description </h1>
+                    </div>
+                    <div class="row" id="row1">
+                        <h1>Pictures </h1>
+                    </div>
                 </div>
-                <div class="col-sm-3" id="col1">
-                    <h1>Team Name</h1>
-                </div>
-                <div class="col-sm-3" id="col1">
-                    <h1>Team Name</h1>
-                </div>
-                <div class="col-sm-3" id="col1">
-                    <h1>Team Name</h1>
-                </div>
-                <div class="col-sm-3" id="col1">
-                    <h1>Team Name</h1>
-                </div>
-            </div>
-            <div class="row-6">
-            <div class="col-sm-3" id="col1">
-                    <h1>Team Name</h1>
-             </div>
             </div>
         </div>
         
