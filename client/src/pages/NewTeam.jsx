c88e7baa6d8ef3eda66b0a715253f3a120ae92f8
patrykku <patrykku@stud.ntnu.no>
2023-02-20 18:02:36 +0100

    Fetches correct team for different years, with description

--- client/src/pages/NewTeam.jsx
+++ client/src/pages/NewTeam.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react'
 import "../styles/pageStyles/NewTeam.css"
 import teamService from '../_services/team.service'
 
-import NewTeamYears from "./NewTeamYears"
 
 export default function NewTeam() {
-    const defaultYear = 2022; // The default year
-    const defaultTeam = 0; // The default index
+
+    const defaultYear = 2023; // The default year
+    const defaultTeam = 1; // The default index
+
+
+    
     const [selectedYear, setSelectedYear] = useState(defaultYear);
     const [selectedTeam, setSelectedTeam] = useState(defaultTeam);
     const [displayTeam, setDisplayTeam] = useState([]);
@@ -20,6 +23,7 @@ export default function NewTeam() {
         setSelectedTeam(team)    
     }
 
+
     useEffect( () => {
         var showYear = selectedYear;
         var showTeam = selectedTeam;
@@ -30,42 +34,56 @@ export default function NewTeam() {
         }
         loadingData(showYear, showTeam);
     }, [selectedTeam, selectedYear])
+
     return(
         <div class="container" id="teamContainer">
             <div class="row">
-                <div class="col-2" id="row1">
-                    <NewTeamYears/>
+                <div class="col-2" id="teamYearCol">
+                    <div id="yearTitle">
+                        <p onClick={() => onClickChangeYear(2023)}>Team 2023</p>
+                    </div>
+                    <div id="yearTitle">
+                        <p onClick={() => onClickChangeYear(2022)}>Team 2022</p>
+                    </div>
+                    <div id="yearTitle">
+                        <p onClick={() => onClickChangeYear(2021)}>Team 2021</p>
+                    </div>
+                    <div id="yearTitle">
+                        <p onClick={() => onClickChangeYear(2020)}>Team 2020</p>
+                    </div>
                 </div>
                 <div class="col-10" id="row1">
                     <div class="row" id="teamTitleRow">
-                        <h1 id="teamTitleText">Our Team </h1>
+                        <h1 id="teamTitleText" >Our Team </h1>
                     </div>
                     <div class="row justify-content-center" id="teamGroupRow">
                     
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Board</p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(0)}>Board</p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Autonomous </p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(1)}>Autonomous </p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Design </p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(2)}>Design </p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Electrical </p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(3)}>Electrical </p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Marketing & Finance</p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(4)}>Marketing & Finance</p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Mechanical </p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(5)}>Mechanical </p>
                         </div>
                         <div class="col-auto" id="chooseGroupButton">
-                            <p id="chooseGroupButtonText">Software </p>
+                            <p id="chooseGroupButtonText" onClick={() => onClickChangeTeam(6)}>Software </p>
                         </div>
                     </div>
                     <div class="row" id="teamdescriptionRow">
-                        <p>Team description </p>
+                        {displayTeam.map(team => (
+                        <p>{team.description} </p>
+                        ))}
                     </div>
                     <div class="row" id="teamPicturesRow">
                         <h1> Pictures </h1>
