a68065de182495f92c74c407b557df90db2a6516
Alice <alicezheng888@gmail.com>
2023-03-13 09:31:55 +0100

    started on un goals

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,10 +6,66 @@ import impact3 from "../assets/pages/about/impact3.png";
 
 import right1 from "../assets/pages/about/aboutValue.jpg";
 
-import  "../styles/pageStyles/about/about.css";
+import "../styles/pageStyles/about/about.css";
+import { SiJavascript } from "react-icons/si";
 
 export default function About() {
-    return(
+
+    return (
+        // <div id="aboutUs">
+        //     <div className="fullCentered">
+        //         <div id="titleAboutUs">
+        //             <h1>About Us</h1>
+        //         </div>
+        //         <div id="subtitleAboutUs">
+        //             <h2>Who We Are</h2>
+        //             <p>Fuel Fighter is a technical student organization at NTNU working with the goal of making the world’s most energy efficient electric car. For 13 years we have been competing in the world’s leading student competition on energy efficiency, the Shell Eco Marathon. With several accomplishments through the years, we are always competing for the first place in the Urban Concept battery electric class, as well as challenging the communication, innovation and design awards. The last two years we have also started developing an autonomous system for the car, for competing in the Shell Eco Marathon Autonomous competition.</p>
+        //         </div>
+        //     </div>
+
+        //     <div id="secondRowText" className="rowTextHalf">
+        //         <p>
+        //             Our firm’s culture is rooted in our core principles. 
+        //         </p>
+        //         <p>
+        //             Here, you will join diverse and 
+        //         </p>
+        //         <p>
+        //             inclusive teams that support each other and
+        //         </p>
+        //         <p>
+        //             empower you to  do  your  best  work.
+        //         </p>
+        //     </div>
+        //     <div id="secondRowImg">
+        //         <img src={right1} />
+        //     </div>
+        //     <div id="aboutImpact">
+        //         <h2>Our Impact</h2>
+        //         <div id="theseImpacts">
+        //             <ValueCard title="Something something" img={impact1} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
+        //             <ValueCard title="Environmental Sustainability" img={impact2} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
+        //             <ValueCard title="Something something" img={impact3} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
+        //         </div>
+        //     </div>
+        //     <div id="fourthRowImg">
+        //         <img src={right1} />
+        //     </div>
+        //     <div id="fourthRowText" className="rowTextHalf">
+        //         <p>
+        //             Our firm’s culture is rooted in our core principles. 
+        //         </p>
+        //         <p>
+        //             Here, you will join diverse and 
+        //         </p>
+        //         <p>
+        //             inclusive teams that support each other and
+        //         </p>
+        //         <p>
+        //             empower you to  do  your  best  work.
+        //         </p>
+        //     </div>
+        // </div>
         <div id="aboutUs">
             <div className="fullCentered">
                 <div id="titleAboutUs">
@@ -20,12 +76,18 @@ export default function About() {
                     <p>Fuel Fighter is a technical student organization at NTNU working with the goal of making the world’s most energy efficient electric car. For 13 years we have been competing in the world’s leading student competition on energy efficiency, the Shell Eco Marathon. With several accomplishments through the years, we are always competing for the first place in the Urban Concept battery electric class, as well as challenging the communication, innovation and design awards. The last two years we have also started developing an autonomous system for the car, for competing in the Shell Eco Marathon Autonomous competition.</p>
                 </div>
             </div>
+            <div id="unGoals">
+                <h1>UN Sustainable Development Goals</h1>
+                <div id="mottoBeskrivelse">
+                    <p>Our motto is “inspire for a sustainable future”. We want to show that working on a project can be done with an environmental mindset, and that the electric car is the future. With more focus on energy efficiency, electric cars can increase their range a lot more than what current models can achieve. To help stop the climate changes within 2030, we focus our work around seven of UN’s 17 sustainable development goals.</p>
+                </div>
+            </div>
             <div id="secondRowText" className="rowTextHalf">
                 <p>
-                    Our firm’s culture is rooted in our core principles. 
+                    Our firm’s culture is rooted in our core principles.
                 </p>
                 <p>
-                    Here, you will join diverse and 
+                    Here, you will join diverse and
                 </p>
                 <p>
                     inclusive teams that support each other and
@@ -40,9 +102,9 @@ export default function About() {
             <div id="aboutImpact">
                 <h2>Our Impact</h2>
                 <div id="theseImpacts">
-                    <ValueCard title="Something something" img={impact1} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
-                    <ValueCard title="Environmental Sustainability" img={impact2} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
-                    <ValueCard title="Something something" img={impact3} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow."/>
+                    <ValueCard title="Something something" img={impact1} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow." />
+                    <ValueCard title="Environmental Sustainability" img={impact2} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow." />
+                    <ValueCard title="Something something" img={impact3} subtitle="Through lending, investment and philanthropic capital, we’re supporting affordable housing and helping the underserved communities of today become flourishing communities tomorrow." />
                 </div>
             </div>
             <div id="fourthRowImg">
@@ -50,10 +112,10 @@ export default function About() {
             </div>
             <div id="fourthRowText" className="rowTextHalf">
                 <p>
-                    Our firm’s culture is rooted in our core principles. 
+                    Our firm’s culture is rooted in our core principles.
                 </p>
                 <p>
-                    Here, you will join diverse and 
+                    Here, you will join diverse and
                 </p>
                 <p>
                     inclusive teams that support each other and
@@ -66,10 +128,10 @@ export default function About() {
     )
 }
 
-const ValueCard = ({img, title, subtitle}) => {
-    return(
+const ValueCard = ({ img, title, subtitle }) => {
+    return (
         <div className="valueCard">
-            <img id="" alt="Picture missing " src={img}/>
+            <img id="" alt="Picture missing " src={img} />
             <h3>{title}</h3>
             <p>{subtitle}</p>
         </div>
