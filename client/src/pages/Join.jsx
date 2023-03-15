7fbaaa94fd90115816ad7e94f58ec969cb1203da
andrerit <andrerit@stud.ntnu.no>
2023-03-15 21:28:19 +0100

    week 11

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import joinHero from '../assets/pages/join/joinHero.jpeg';
 import '../styles/pageStyles/join.css';
