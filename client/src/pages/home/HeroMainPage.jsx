42d38a83bc39591bce737fd954c4a87c927eb6f3
patrykku <patrykku@stud.ntnu.no>
2023-04-17 17:29:03 +0200

    Updated home page to show new animation

--- client/src/pages/home/HeroMainPage.jsx
+++ client/src/pages/home/HeroMainPage.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 import "../../styles/pageStyles/home/heroHome.css";
 
-import video from '../../assets/video/mainPageVideo.mp4';
+//import video from '../../assets/video/mainPageVideo.mp4';
 import landingImage from '../../assets/pages/home/car_render_transparent_crop1.png';
+import video from '../../assets/video/mainPageAnimation.mp4';
 
 export default function HeroHome() {
     const [videoVisible, setVideoVisible] = useState(true)
@@ -14,11 +15,12 @@ export default function HeroHome() {
                 <div id="homeTitle" className="homeTitle">
                     <h1>Fuel Fighter NTNU</h1>
                 </div>
-                {videoVisible ?
+                {/* {videoVisible ?
                     <video src={video} onEnded={() => setVideoVisible(false)} controls={false} autoPlay muted />
                     :
                     <img id="landingimage" src={landingImage} alt="fuel fighter" />
-                }
+                } */}
+                <video src={video} onEnded={() => setVideoVisible(false)} controls={false} autoPlay muted />
             </div>
         </section>
     )
